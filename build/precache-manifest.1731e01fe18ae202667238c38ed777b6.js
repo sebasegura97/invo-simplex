d016f3801821d717ea423a7e4bc935d5254f4261
Sebastian Segura <sebasegura97@gmail.com>
2019-06-13 09:50:56 -0300

    a

---
+++ build/precache-manifest.1731e01fe18ae202667238c38ed777b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f138a1c812c0dbb0140ec877ac7f6f",
+    "url": "/invo-simplex/index.html"
+  },
+  {
+    "revision": "8b82620baa1c86b184a2",
+    "url": "/invo-simplex/static/css/main.3c97b726.chunk.css"
+  },
+  {
+    "revision": "e2b5ac78f4a55096da8b",
+    "url": "/invo-simplex/static/js/2.10d10230.chunk.js"
+  },
+  {
+    "revision": "8b82620baa1c86b184a2",
+    "url": "/invo-simplex/static/js/main.07509b89.chunk.js"
+  },
+  {
+    "revision": "b2cdc316410cc6f0b44e",
+    "url": "/invo-simplex/static/js/runtime~main.b11ac85c.js"
+  }
+]);
